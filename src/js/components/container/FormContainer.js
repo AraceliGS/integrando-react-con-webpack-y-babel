e0f64e67d2e76d4b88e3fa64249790dbb09016ec
AraceliGS <aragutarras@gmail.com>
2018-03-01 12:39:40 -0500

    añadiendo componente FormContainer

--- src/js/components/container/FormContainer.js
+++ src/js/components/container/FormContainer.js
@@ -1,2 +1,20 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom'; 
+
+class FormContainer extends Component {
+  contructor() {
+    super();
+
+    this.state = {
+      title:''
+    };
+  }
+
+  render()  {
+    return (
+      <form id='article-form'></form>
+    );
+  }
+}
+
+export default FormContainer;
